a52a3b028e2cf2aca00475d4e0e0b591d9586fdd
smiciklaslearning <email@smiciklaslearning.com>
2023-04-12 19:32:50 -0400

    Single Page

--- src/components/body.js
+++ src/components/body.js
@@ -2,13 +2,16 @@ import { InlineWidget } from "react-calendly";
 
 export default function Body () { return (
 <div>
-    <a href="/"><img src="logolettering.jpg" alt=""></img></a>
+    <div id="header">
+        <img src="logolettering.jpg" style={{ height: "100" }} alt=""></img>
+    </div>
     <div id="book">
         <InlineWidget url="https://calendly.com/smiciklaslearning" styles={{height: '800px'}} pageSettings={{hideGdprBanner: true}}/>   
     </div>
     <div id="video">
         <iframe width="100%" height="100%" src="https://www.youtube.com/embed/C-Pkp7aJrfQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
+    <div id="footer">201-736-1268 <br/> email@smiciklaslearning.com</div>
 </div>
 )
 }
