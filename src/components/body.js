87cd47ca98f8276bb5bab5e6bd0ea22967efd5bc
smiciklaslearning <email@smiciklaslearning.com>
2023-04-13 14:19:51 -0400

    Single Page

--- src/components/body.js
+++ src/components/body.js
@@ -1,17 +1,38 @@
-import { InlineWidget } from "react-calendly";
-
 export default function Body () { return (
 <div>
     <div id="header">
-        <img src="logolettering.jpg" style={{ height: "100" }} alt=""></img>
+        <table>
+            <tr>
+                <th></th>
+                <th>Calendly</th>
+                <th>LinkedIn</th>
+                <th>YouTube</th>
+                <th>Facebook</th>
+                <th>Github</th>
+            </tr>
+            <tr>
+                <td><a href="/"><img src="logolettering.jpg" style={{ height: "80" }} alt=""></img></a></td>
+                <td><a href="https://calendly.com/smiciklaslearning"><img src="calendly.jpg" style={{ height: "80", width: "80" }} alt=""></img></a></td>
+                <td><a href="https://www.linkedin.com/in/andrew-smiciklas-42a4b3272/"><img src="linkedin.jpg" style={{ height: "80", width: "80" }} alt=""></img></a></td>
+                <td><a href="https://www.youtube.com/@smiciklaslearning5336"><img src="youtube.png" style={{ height: "80", width: "80" }} alt=""></img></a></td>
+                <td><a href="https://www.facebook.com/smiciklaslearning"><img src="facebook.png" style={{ height: "80", width: "80" }} alt=""></img></a></td>
+                <td><a href="https://github.com/andr00js"><img src="github.jpg" style={{ height: "80", width: "80" }} alt=""></img></a></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>Schedule A Free Session</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>        
+        </table>
     </div>
-    <div id="book">
-        <InlineWidget url="https://calendly.com/smiciklaslearning" styles={{height: '800px'}} pageSettings={{hideGdprBanner: true}}/>   
+    <div id="center"><img src="ninetyone.jpg" style={{ height: "50%", width: "40%" }} alt=""></img></div>
+    <div id="footer">
+        <span id="sidebyside">CALL OR TEXT: 201-736-1268</span> 
+        <span id="sidebyside">EMAIL: email@smiciklaslearning.com</span>
     </div>
-    <div id="video">
-        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/C-Pkp7aJrfQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-    </div>
-    <div id="footer">201-736-1268 <br/> email@smiciklaslearning.com</div>
 </div>
 )
 }
