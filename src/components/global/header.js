a0f1bc11b774dcd975d9915bba060a1ddfa5103e
smiciklaslearning <email@smiciklaslearning.com>
2023-04-16 09:43:28 -0400

    Single Page

--- src/components/global/header.js
+++ src/components/global/header.js
@@ -12,12 +12,12 @@ export default function Header () { return (
                     <th>Github</th>
                 </tr>
                 <tr>
-                    <td><a href="/"><img src="logolettering.jpg" style={{ height: "80" }} alt=""></img></a></td>
-                    <td><a href="https://calendly.com/smiciklaslearning"><img id="pulse"src="calendly.jpg" style={{ height: "80", width: "80" }} alt=""></img></a></td>
-                    <td><a href="https://www.linkedin.com/in/andrew-smiciklas-42a4b3272/"><img src="linkedin.jpg" style={{ height: "80", width: "80" }} alt=""></img></a></td>
-                    <td><a href="https://www.youtube.com/@smiciklaslearning5336"><img src="youtube.png" style={{ height: "80", width: "80" }} alt=""></img></a></td>
-                    <td><a href="https://www.facebook.com/smiciklaslearning"><img src="facebook.png" style={{ height: "80", width: "80" }} alt=""></img></a></td>
-                    <td><a href="https://github.com/andr00js"><img src="github.jpg" style={{ height: "80", width: "80" }} alt=""></img></a></td>
+                    <td><a href="/"><img src="logolettering.jpg" style={{ width: '80%', height: '80%' }} alt=""></img></a></td>
+                    <td><a href="https://calendly.com/smiciklaslearning"><img id="pulse"src="calendly.jpg" style={{ width: '30%', height: '30%' }} alt=""></img></a></td>
+                    <td><a href="https://www.linkedin.com/in/andrew-smiciklas-42a4b3272/"><img src="linkedin.jpg" style={{ width: '30%', height: '30%' }} alt=""></img></a></td>
+                    <td><a href="https://www.youtube.com/@smiciklaslearning5336"><img src="youtube.png" style={{ width: '30%', height: '30%' }} alt=""></img></a></td>
+                    <td><a href="https://www.facebook.com/smiciklaslearning"><img src="facebook.png" style={{ width: '30%', height: '30%' }} alt=""></img></a></td>
+                    <td><a href="https://github.com/andr00js"><img src="github.jpg" style={{ width: '30%', height: '30%' }} alt=""></img></a></td>
                 </tr>
                 <tr>
                     <td>email@smiciklaslearning.com</td>
