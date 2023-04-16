e052372f99d98d3190cdb06933dd70e94351db24
smiciklaslearning <email@smiciklaslearning.com>
2023-04-16 10:12:58 -0400

    Single Page

--- src/components/global/header.js
+++ src/components/global/header.js
@@ -13,11 +13,11 @@ export default function Header () { return (
                 </tr>
                 <tr>
                     <td><a href="/"><img src="logolettering.jpg" style={{ width: 200, height: 70 }} alt=""></img></a></td>
-                    <td><a href="https://calendly.com/smiciklaslearning"><img id="pulse"src="calendly.jpg" style={{ width: 50, height: 50 }} alt=""></img></a></td>
-                    <td><a href="https://www.linkedin.com/in/andrew-smiciklas-42a4b3272/"><img src="linkedin.jpg" style={{ width: 50, height: 50 }} alt=""></img></a></td>
-                    <td><a href="https://www.youtube.com/@smiciklaslearning5336"><img src="youtube.png" style={{ width: 50, height: 50 }} alt=""></img></a></td>
-                    <td><a href="https://www.facebook.com/smiciklaslearning"><img src="facebook.png" style={{ width: 50, height: 50 }} alt=""></img></a></td>
-                    <td><a href="https://github.com/andr00js"><img src="github.jpg" style={{ width: 50, height: 50 }} alt=""></img></a></td>
+                    <td><a href="https://calendly.com/smiciklaslearning"><img id="pulse"src="calendly.jpg" style={{ width: 70, height: 70 }} alt=""></img></a></td>
+                    <td><a href="https://www.linkedin.com/in/andrew-smiciklas-42a4b3272/"><img src="linkedin.jpg" style={{ width: 70, height: 70 }} alt=""></img></a></td>
+                    <td><a href="https://www.youtube.com/@smiciklaslearning5336"><img src="youtube.png" style={{ width: 70, height: 70 }} alt=""></img></a></td>
+                    <td><a href="https://www.facebook.com/smiciklaslearning"><img src="facebook.png" style={{ width: 70, height: 70 }} alt=""></img></a></td>
+                    <td><a href="https://github.com/andr00js"><img src="github.jpg" style={{ width: 70, height: 70 }} alt=""></img></a></td>
                 </tr>
                 <tr>
                     <td>email@smiciklaslearning.com</td>
