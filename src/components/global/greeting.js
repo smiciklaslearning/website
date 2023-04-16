bc7ebfe828111df3945614e3fe16b611a20c7093
smiciklaslearning <email@smiciklaslearning.com>
2023-04-16 10:35:57 -0400

    Single Page

--- src/components/global/greeting.js
+++ src/components/global/greeting.js
@@ -6,6 +6,7 @@ export default function Greeting () { return (
             <p id="text">Click the blue Calendly icon at the top of the page to schedule your free lesson.</p>
             <p id="text">My standard rates are $30 for 30 minutes, and $50 for 1 hour. <br/> No contracts or commitments!</p>
             <p id="text">Call or text (201) 736-1268 to get started. <br/>You can also send an email to email@smiciklaslearning.com</p>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/C-Pkp7aJrfQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
         <div id="greeting">
             <div id="title">COURSES</div> 
